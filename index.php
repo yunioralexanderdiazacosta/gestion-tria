15d0151e92384a6a88d9c2686d89a9437952ff12
yunioralexanderdiazacosta <yuniordiaz1@gmail.com>
2022-03-10 08:10:57 -0400

    fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['usuario'])) {
         $reprobados                 = $row3['reprobados'];
         $total                      = $aprobados + $no_entregados + $reprobados;
         $entregados                 = $aprobados + $reprobados;
-        $porcentaje                 = $total > 0 ? round($entregados / $total) * 100 : 0;
+        $porcentaje                 = $total > 0 ? round($entregados / $total * 100) : 0;
     }
 ?>
     <!DOCTYPE html>
@@ -363,6 +363,7 @@ if (isset($_SESSION['usuario'])) {
             $('#dataTable').DataTable({
                 language:{sProcessing:"Procesando...",sLengthMenu:"Mostrar _MENU_ registros",sZeroRecords:"No se encontraron resultados",sEmptyTable:"Ningún dato disponible en esta tabla",sInfo:"Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",sInfoEmpty:"Mostrando registros del 0 al 0 de un total de 0 registros",sInfoFiltered:"(filtrado de un total de _MAX_ registros)",sInfoPostFix:"",sSearch:"Buscar:",sUrl:"",sInfoThousands:",",sLoadingRecords:"Cargando...",oPaginate:{sFirst:"Primero",sLast:"Último",sNext:"Siguiente",sPrevious:"Anterior"},oAria:{sSortAscending:": Activar para ordenar la columna de manera ascendente",sSortDescending:": Activar para ordenar la columna de manera descendente"},buttons:{print:"Imprimir"}}
             });
+            obtener_estudiantes();
             $('.selectpicker').selectpicker();
             $('[data-toggle="tooltip"]').tooltip();
         });
